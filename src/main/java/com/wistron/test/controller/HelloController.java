51e02d5fa89b7a3be66ef3a638b716515b2e08f9
ZengYH5946 <gws-rhz8cxk2jku@GWS.ALIYUN>
2020-09-09 11:39:05 +0800

    zzz

--- src/main/java/com/wistron/test/controller/HelloController.java
+++ src/main/java/com/wistron/test/controller/HelloController.java
@@ -5,29 +5,42 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.wistron.test.entity.Student;
 
+@CrossOrigin//解决跨域问题
 @RestController
 public class HelloController {
-	
 	@RequestMapping("/hello")
 	public Map<String, Student> hello() {
-		Map<String, Student> map = new HashMap<String, Student>();
-		Student stu = new Student(0, "zorro", "男");
-		map.put("1", stu);
-		map.put("2", stu);
+		Map<String, Student> map = new HashMap<>();
+		Student stu = new Student(0, "ZengYH", "男");
+		Student stu2 = new Student(1, "ZengYH_Zorro", "男");
+		map.put("0", stu);
+		map.put("1", stu2);
 		return map;
 	}
 	
 	@RequestMapping("/haha")
 	public List<Student> haha() {
-		List<Student> list = new ArrayList<Student>();
+		List<Student> list = new ArrayList<>();
 		Student stu = new Student(0, "zorro", "男");
 		list.add(0, stu);
 		list.add(1, stu);
 		return list;
+	}
+
+	@RequestMapping("/zzz")
+	public Map<String, Object> zzz() {
+		Map<String, Object> map = new HashMap<>();
+		List<Student> list = new ArrayList<>();
+		Student stu = new Student(0, "zorro", "男");
+		list.add(0, stu);
+		list.add(1, stu);
+		map.put("test", list);
+		return map;
 	}
 }
